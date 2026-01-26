4005b3e5ea06d8a9947d20e528a8f226f5542413
Anatoly <samorazvitie02092025@gmail.com>
2026-01-27 00:42:39 +0500

    2. Array, loops. min/max program with array

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Задача 1 (код)
-        System.out.println("Введите защитный код длиной 4!:");
         Scanner in = new Scanner(System.in);
-        short code = in.nextShort();
-        in.nextLine();
-        if (code >= 1000 && code <= 9999) {
-            short key = 147;
-            short encrypted = (short) (code ^ key);
-            System.out.printf("Зашифрованный код: %d \n", encrypted);
-            short decrypted = (short) (encrypted ^ key);
-            System.out.printf("Расшифрованный код: %d", decrypted);
-        }
-        else
-            System.out.println("ОШИБКА! Код не длины 4!");
 
+        int[] nums = new int[10];
+        System.out.println("Заполните массив любыми 10-ю цифрами:");
+        for (int i = 0; i < nums.length; i++){
+            int number = in.nextInt();
+            nums[i] = number;
+        }
+
+        int largest = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+
+        System.out.println("Максимальное число массива nums: " + largest);
     }
 }
