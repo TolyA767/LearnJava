2af2166a2241f4dbea2338610b971894211cbe43
Anatoly <samorazvitie02092025@gmail.com>
2026-02-06 01:12:44 +0500

    6. Created classes: Student, Calculator, and Car. The factorial method. I recalled constructors, method overloading, and static fields and methods.

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,14 @@
-import java.util.Date;
 public class Main {
+    public static int fact(int n) {
+        if (n == 1)
+            return 1;
+        return n * fact(n-1);
+    }
     public static void main(String[] args) {
-        Person anna = new Person("Anna");
-        System.out.println(anna.cnt);
-        Person olya = new Person("Olya");
-        System.out.println(olya.cnt);
+        int n = 5;
 
-        int res = Calculator.Sum(5, 10);
+        int res = fact(n);
         System.out.println(res);
 
-        Date curDate = new Date();
-        System.out.println(curDate);
-    }
-}
-
-class Person {
-    String name;
-    static int cnt = 0;
-
-    Person(String name) {
-        this.name = name;
-        cnt += 1;
-    }
-}
-
-class Calculator {
-
-    static int Sum(int a, int b) {
-        return a + b;
-    }
-
-    int Sum2(int a, int b) {
-        return a + b;
     }
 }
